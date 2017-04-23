718f786bc8888749af3f835a7ee88341ef29cb5d
Shivam Bansal <bshivam4@gmail.com>
2017-04-23 13:37:21 +0530

    add notification -5

--- app/Http/Controllers/IndividualsController.php
+++ app/Http/Controllers/IndividualsController.php
@@ -55,7 +55,8 @@ class IndividualsController extends Controller
             'image' => 'mimes:jpeg,bmp,png,jpg|max:5120'
         ]);
 
-        $filename=asset('storage/').$request->mobile."_PP";
+        $filename=asset('storage/').$request->mobile."_PP"->$request->image->getE;
+
         Individual::create([
             'name' => $request->name,
             'gender' =>  $request->gender,
